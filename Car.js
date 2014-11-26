53a4733b6c19771705041dacd895ce4cf0f054e5
Nick Lathe <nicklathe@icloud.com>
2014-11-25 17:09:12 -0800

    Finished assignment. All tests complete.

--- Car.js
+++ Car.js
@@ -1,24 +1,74 @@
-function Car(make, model, year, color, seats){
+function Car(make, model, year, color, seats, passengers){
   this.make = make;
   this.model = model;
-  //TODO: add year, color, seats here
-
+  this.year = year;
+  this.color = color;
+  this.seats = seats;
   this.running = false;
   this.owner = "manufacturer";
   this.previousOwners = [];
+  this.passengers = (passengers) ? passengers : [];
 }
 
 Car.prototype.sell = function(newOwner){
-
+	this.previousOwners.push(this.owner);
+	this.owner = newOwner;
 };
 
 Car.prototype.paint = function(newColor){
-
+	this.color = newColor;
 };
 
+Car.prototype.start = function(){
+	this.running = true;
+}
 
+Car.prototype.off = function(){
+	this.running = false;
+}
 
+Car.prototype.driveTo = function(destination){
+	if(this.running === true) {
+		console.log("driving to " + destination);
+		return true;
+	} else {
+		return false;
+	}
+}
 
+Car.prototype.park = function(){
+	if(this.running === false) {
+		console.log("parked!!");
+		return true;
+	} else {
+		return false;
+	}
+}
+
+Car.prototype.pickUp = function(name){
+	if(this.running === true && this.passengers.length < this.seats - 1){
+		console.log("driving to 'pick up " + name + "'");
+		this.passengers.push(name);
+		return true;
+	} else {
+		return false;
+	}
+}
+
+Car.prototype.dropOff = function(name){
+	var personX = this.passengers.indexOf(name);
+	if(this.running && personX != -1) {
+		console.log("driving to drop off " + name);
+		this.passengers.splice(personX, 1);
+		return true;
+	} else {
+		return false;
+	}
+}
+
+Car.prototype.passengerCount = function(){
+	return this.passengers.length;
+}
 
 
 
